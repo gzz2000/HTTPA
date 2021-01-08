c303a1e36909a72d69ebc365a6e1976986469276
GZZ <gzz_2000@126.com>
2021-01-08 17:29:34 +0800

    proxy works occasionally. abandoning

--- extension/background.js
+++ extension/background.js
@@ -1,15 +1,12 @@
 
 function onBeforeRequest(req) {
-  // console.log("requesting: " + req.url, req);
+  console.log("requesting: " + req.url, req);
   
   if(req.url == 'http://nossl.cn/') {
-    return {'redirectUrl': 'https://guozz.cn/'};
+    return {'redirectUrl': 'https://guozz.cn/',
+            'rewrite': true};
   }
   return {};
-}
-
-function onBeforeSendHeaders() {
-  
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
@@ -18,8 +15,24 @@ chrome.webRequest.onBeforeRequest.addListener(
   ["blocking"]
 );
 
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  onBeforeSendHeaders,
-  {urls: ["<all_urls>"]},
-  ["blocking"]
-);
+function FindProxyForURL(url, host) {
+  if(host == 'auth.localhost') return 'PROXY 10.0.99.88';
+  else if(host == 'isitwork') return 'PROXY 127.0.0.1';
+  else return 'DIRECT';
+}
+
+chrome.proxy.settings.set({
+  value: {
+    mode: 'fixed_servers',
+    rules: {
+      proxyForHttp: {
+        scheme: 'http',
+        host: '127.0.0.2'
+      },
+      bypassList: ['<-loopback>'],
+    }
+    /* pacScript: {
+     *   data: FindProxyForURL.toString()
+     * }, */
+  }
+});
