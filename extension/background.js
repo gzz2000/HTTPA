32468f1d72f2c9378102cb72c9054ace972583e2
GZZ <gzz_2000@126.com>
2021-01-08 17:41:46 +0800

    Eliminate that proxy

--- extension/background.js
+++ extension/background.js
@@ -14,25 +14,3 @@ chrome.webRequest.onBeforeRequest.addListener(
   {urls: ["<all_urls>"]},
   ["blocking"]
 );
-
-function FindProxyForURL(url, host) {
-  if(host == 'auth.localhost') return 'PROXY 10.0.99.88';
-  else if(host == 'isitwork') return 'PROXY 127.0.0.1';
-  else return 'DIRECT';
-}
-
-chrome.proxy.settings.set({
-  value: {
-    mode: 'fixed_servers',
-    rules: {
-      proxyForHttp: {
-        scheme: 'http',
-        host: '127.0.0.2'
-      },
-      bypassList: ['<-loopback>'],
-    }
-    /* pacScript: {
-     *   data: FindProxyForURL.toString()
-     * }, */
-  }
-});
