7cd33bf6e2860c6d1c0cc32778734793216ed0ae
GZZ <gzz_2000@126.com>
2021-01-08 20:53:28 +0800

    Move chrome app implementation to separate folder

---
+++ client/obsolete/ext-firefox-nightly/protocol.js
@@ -0,0 +1,78 @@
+
+browser.protocol.registerProtocol("dweb", request => {
+  const examples = ["stream", "async", "crash", "text", "html"]
+  switch (request.url) {
+    case "dweb://stream/": {
+      let cancelled = false
+      const body = new ReadableStream({
+        async start(controller) {
+          controller.enqueue("<h1>Say Hi to endless stream!</h1>\n")
+          let n = 0
+          while (!cancelled && n < 10) {
+            await new Promise(resolve => setTimeout(resolve, 1000))
+            controller.enqueue(`<p>Chunk #${++n}<p>`)
+          }
+          controller.close()
+        },
+        cancel() {
+          cancelled = true
+        }
+      })
+      return new Response(body, {
+        headers: { "content-type": "text/html;charset=utf-8" }
+      })
+    }
+    case "dweb://async/": {
+      return new Promise((resolve, reject) => {
+        setTimeout(
+          resolve,
+          100,
+          new Response("Async response yo!", {
+            headers: {
+              "content-type": "text/plain"
+            }
+          })
+        )
+      })
+    }
+    case "dweb://crash/": {
+      throw Error("Boom!")
+    }
+    case "dweb://text/": {
+      return new Response("Just a plain text")
+    }
+    case "dweb://html/": {
+      const blob = new Blob([
+        "<h1>HTML</h1>",
+        "<p>ContentType was inferred as HTML"
+      ])
+      return new Response(blob)
+    }
+    case "dweb://echo/": {
+      return (async () => {
+        const data = await request.text()
+        return new Response(`echo: ${data}`)
+      })()
+    }
+    default: {
+      const body = new ReadableStream({
+        start(controller) {
+          controller.enqueue("<h1>Hi there!</h1>\n")
+          controller.enqueue(`<p>You've succesfully loaded <strong>${
+            request.url
+          }</strong><p>
+          ${examples
+            .map(ex => `<a href=\"dweb://${ex}/\" >${ex}</a>`)
+            .join("<br>")}`)
+          controller.close()
+        }
+      })
+
+      return new Response(body, {
+        headers: {
+            "content-type": "text/html"
+        }
+      })
+    }
+  }
+});
