0c5fe68518ff54083a66387cc959288f93bfcc08
Yukimai <yukimaikoriya@vip.qq.com>
2021-01-10 14:17:56 +0800

    added simulation website apple.com

---
+++ websites/mksite.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys, os, re
+
+reStatic = re.compile(r'"\./([^/]+)/(.+?)"')
+reDynamic = re.compile(r'''http(s?)://([^/]+)([/'"])''')
+reOther = re.compile(r'(\.下载|\.download)"')
+subStatic = r'"../static/{}/\2?:HTTPA"'
+subDynamic = r'http\1://error\3'
+subOther = r'"'
+
+
+def main(sitename):
+    os.mkdir(sitename)
+    os.mkdir(os.path.join('./static', sitename))
+    for fn in os.listdir('./download'):
+        if fn.endswith('.html'):
+            with open(os.path.join('./download', fn), 'r', encoding='utf-8') as hin, open(os.path.join(sitename, 'index.html'), 'w', encoding='utf-8') as hout:
+                content = hin.read()
+                content = reOther.sub(subOther, content)
+                content = reStatic.sub(subStatic.format(sitename), content)
+                content = reDynamic.sub(subDynamic, content)
+                hout.write(content)
+        else:
+            curdir = os.path.join('./download', fn)
+            for stn in os.listdir(curdir):
+                inpath = os.path.join(curdir, stn)
+                if stn.endswith('.下载'):
+                    stn = stn[:-3]
+                elif stn.endswith('.download'):
+                    stn = stn[:-9]
+                oupath = os.path.join('./static', sitename, stn)
+                with open(inpath, 'rb') as fin, open(oupath, 'wb') as fout:
+                    fout.write(fin.read())
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage: ./mksite.py website.com')
+    else:
+        main(sys.argv[1])
